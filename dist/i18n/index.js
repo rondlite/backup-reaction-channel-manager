bca6107fccc63c789cd6b292e361130653a97355
demandJS_France <demandjs@sd-60174.dedibox.fr>
2020-05-10 21:11:49 +0200

    removed babel

--- dist/i18n/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _en = _interopRequireDefault(require("./en.json"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var _default =
-
-{
-  translations: [
-  ..._en.default] };exports.default = _default;
--- dist/schemas/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _importAsString = _interopRequireDefault(require("@reactioncommerce/api-utils/importAsString"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-const schema = (0, _importAsString.default)("./schemas.graphql");var _default =
-
-[schema];exports.default = _default;
